f6d2f24a0b63958737c72d8145b4ee0f420bd99a
bintepasharifat <bintepasharifat@gmail.com>
2025-02-13 21:16:14 +0600

    readme.md file update

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,21 @@ import 'package:todo_app/home.dart';
 import 'package:todo_app/provider/todo_provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_)=>TodoProvider())
-  ],
-  child: const MyApp(),)
-  );
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => TodoProvider())],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
